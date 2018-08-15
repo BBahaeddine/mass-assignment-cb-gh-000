8e658dceaac380488d72dc5cd63a12390c7c4475
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-15 11:15:17 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
   #your code here
   def initialize(args)
-    args.each{|key, value| self.send("#{key}=", value)}
+    args.each{|key, value| self.send(("#{key}="), value)}
   end
 end
